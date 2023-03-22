fafe18c6fbc7609450ee81e32800d8186be20e2e
AntonyIS <antonyshikubu@gmail.com>
2023-03-22 22:09:47 +0300

    minor changes

--- adapters/database/dynamodb/dynamodb.go
+++ adapters/database/dynamodb/dynamodb.go
@@ -1,4 +1,4 @@
-package database
+package dynamodb
 
 import (
 	"errors"
--- core/services/services.go
+++ core/services/services.go
@@ -19,71 +19,71 @@ var (
 )
 
 type appService struct {
-	repo *ports.VisionRepository
+	repo ports.VisionRepository
 }
 
-func NewService(repo *ports.VisionRepository) *appService {
-	return &appService{
+func NewService(repo ports.VisionRepository) appService {
+	return appService{
 		repo: repo,
 	}
 }
 
-func (a *appService) CreateUser(user *domain.User) (*domain.User, error) {
-	return nil, nil
+func (a appService) CreateUser(user *domain.User) (*domain.User, error) {
+	return a.repo.CreateUser(user)
 }
 
-func (a *appService) ReadUser(user_id string) (*domain.User, error) {
-	return nil, nil
+func (a appService) ReadUser(user_id string) (*domain.User, error) {
+	return a.repo.ReadUser(user_id)
 }
 
-func (a *appService) ReadUsers() ([]*domain.User, error) {
-	return nil, nil
+func (a appService) ReadUsers() ([]*domain.User, error) {
+	return a.repo.ReadUsers()
 }
 
-func (a *appService) UpdateUser(user *domain.User) (*domain.User, error) {
-	return nil, nil
+func (a appService) UpdateUser(user *domain.User) (*domain.User, error) {
+	return a.repo.UpdateUser(user)
 }
 
-func (a *appService) DeleteUser(user_id string) error {
-	return nil
+func (a appService) DeleteUser(user_id string) error {
+	return a.repo.DeleteUser(user_id)
 }
 
-func (a *appService) CreateDevice(device *domain.Device) (*domain.Device, error) {
-	return nil, nil
+func (a appService) CreateDevice(device *domain.Device) (*domain.Device, error) {
+	return a.repo.CreateDevice(device)
 }
 
-func (a *appService) ReadDevice(device_id string) (*domain.Device, error) {
-	return nil, nil
+func (a appService) ReadDevice(device_id string) (*domain.Device, error) {
+	return a.repo.ReadDevice(device_id)
 }
 
-func (a *appService) ReadDevices() ([]*domain.Device, error) {
-	return nil, nil
+func (a appService) ReadDevices() ([]*domain.Device, error) {
+	return a.repo.ReadDevices()
 }
 
-func (a *appService) UpdateDevice(device *domain.Device) (*domain.Device, error) {
-	return nil, nil
+func (a appService) UpdateDevice(device *domain.Device) (*domain.Device, error) {
+	return a.repo.UpdateDevice(device)
 }
 
-func (a *appService) DeleteDevice(device_id string) error {
-	return nil
+func (a appService) DeleteDevice(device_id string) error {
+	return a.repo.DeleteDevice(device_id)
 }
 
-func (a *appService) CreateClient(client *domain.Client) (*domain.Client, error) {
-	return nil, nil
+func (a appService) CreateClient(client *domain.Client) (*domain.Client, error) {
+	return a.repo.CreateClient(client)
 }
 
-func (a *appService) ReadClient(client_id string) (*domain.Client, error) {
-	return nil, nil
+func (a appService) ReadClient(client_id string) (*domain.Client, error) {
+	return a.repo.ReadClient(client_id)
 }
 
-func (a *appService) ReadClients() ([]*domain.Client, error) {
-	return nil, nil
+func (a appService) ReadClients() ([]*domain.Client, error) {
+	return a.repo.ReadClients()
 }
 
-func (a *appService) UpdateClient(client *domain.Client) (*domain.Client, error) {
-	return nil, nil
+func (a appService) UpdateClient(client *domain.Client) (*domain.Client, error) {
+	return a.repo.UpdateClient(client)
 }
 
-func (a *appService) DeleteClient(client_id string) error {
-	return nil
+func (a appService) DeleteClient(client_id string) error {
+	return a.repo.DeleteClient(client_id)
 }
